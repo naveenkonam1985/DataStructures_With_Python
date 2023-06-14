b616ce43b93280ac802c31e1d803176e46791624
Naveen Kumar Konam <naveenkonam1985@gmail.com>
2023-06-14 17:31:06 +0530

    added queue

---
+++ queues/queue.py
@@ -0,0 +1,45 @@
+# Implementation of Queue 
+# Naveen Kumar Konam
+
+# class for Node
+class Node:
+    def __init__(self, elem, next=None)
+        self.elem = elem
+        self.next = next
+
+
+class Queue:
+    def __init__(self):
+        pass
+
+
+    def insert(self, elem):
+        pass
+
+    
+    def remove(self, elem):
+        pass
+
+
+    def peek(self, elem):
+        pass
+
+    
+    def isEmpty(self):
+        pass
+
+
+    def isFull(self):
+        pass
+
+
+    def __contains__(self, elem):
+        pass
+
+
+    def __len__(self):
+        pass
+
+
+    def __str__(self):
+        return f"This is Queue"
