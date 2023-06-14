fbc69a8e4134848309b61bbfb2ebfa511f7b6653
Naveen Kumar Konam <naveenkonam1985@gmail.com>
2023-06-14 19:05:59 +0530

    Queue with List type added

--- queues/queue.py
+++ queues/queue.py
@@ -1,23 +1,26 @@
 # Implementation of Queue 
 # Naveen Kumar Konam
 
+from typing import List
+
 # class for Node
 class Node:
-    def __init__(self, elem, next=None)
+    def __init__(self, elem, next=None):
         self.elem = elem
         self.next = next
 
 
+# class for Queue
 class Queue:
     def __init__(self):
         pass
 
 
-    def insert(self, elem):
+    def add(self, elem):
         pass
 
     
-    def remove(self, elem):
+    def pop(self):
         pass
 
 
@@ -46,4 +49,62 @@ class Queue:
 
 
     def __str__(self):
-        return f"This is Queue"
+        pass
+
+    def __repr__(self):
+        pass
+
+
+
+class ListQueue(Queue):
+    def __init__(self, elements = None):
+        if elements:
+            if isinstance(elements, List):
+                self.__elements = elements
+                self.__length = len(elements)
+            else:
+                raise Exception("Not a List")
+        else:
+            self.__elements = elements
+            self.__length = 0
+
+    def add(self, elem):
+        self.__elements.append(elem)
+        self.__length += 1
+        return self.__elements
+    
+    def pop(self):
+        if self.isEmpty():
+            return Exception
+        else:
+            self.__length -= 1
+            return self.__elements.pop(0)
+        
+    
+    def isEmpty(self):
+        if len(self.__elements) == 0:
+            return True
+        else:
+            return False
+        
+    def clear(self):
+        self.__elements = list()
+        self.__length = 0
+        
+    def __len__(self):
+        return self.__length
+        
+    def __str__(self):
+        return f"Queue with length {self.__length} and elements {self.__elements}"
+    
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f"{class_name}: elements {self.__elements}"
+
+
+if __name__ == '__main__':
+    customers = ListQueue()
+    customers.add('a')
+    customers.clear()
+
+    print(customers)
