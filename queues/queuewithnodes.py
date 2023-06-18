e31c075460c960e6b05208a7997010c7ac4433a9
Naveen Kumar Konam <naveenkonam1985@gmail.com>
2023-06-18 20:26:30 +0530

    Queues with Linked List file added

---
+++ queues/queuewithnodes.py
@@ -0,0 +1,11 @@
+# Queues implemented with Linked Lists in Python
+# Naveen Kumar Konam
+
+class Node:
+    def __init__(self, data=None):
+        self._data = data
+        self._next = None
+
+
+class QueueWithNodes:
+    pass
