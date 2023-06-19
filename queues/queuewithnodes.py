9d6f3084fc473ef26966060d4581788173dbe839
Naveen Kumar Konam <naveenkonam1985@gmail.com>
2023-06-19 22:35:37 +0530

    added functions

--- queues/queuewithnodes.py
+++ queues/queuewithnodes.py
@@ -3,9 +3,51 @@
 
 class Node:
     def __init__(self, data=None):
-        self._data = data
-        self._next = None
+        self.data = data
+        self.next = None
+        self.prev = None
 
 
 class QueueWithNodes:
-    pass
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+
+    def enqueue(self, elem):
+        new_node = Node(elem)
+        if self.head == None:
+            self.head = new_node
+            self.tail = self.head
+
+        else:
+            new_node.prev=self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.count += 1
+
+    
+    def dequeue(self):
+        if self.count == 1:
+            self.count -= 1
+            self.head = None
+            self.tail = None
+
+        elif self.count > 1:
+            self.count -= 1
+            self.head = self.head.next
+            self.head.prev = None
+
+        elif self.count < 1:
+            print("Queue is Empty")
+        
+        
+
+
+if __name__ == '__main__':
+    q = QueueWithNodes()
+    q.dequeue()
+
+    print("Elements in Queue: ", q.count) 
